a467dec3724999d676a4dd6bf55b8ca43a973402
bennett.chan <bennett.chan@tbcasoft.com>
2022-03-13 17:43:07 +0800

    Initial Commit

---
+++ APIPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  APIPractice
+//
+//  Created by TBCASoft-Bennett on 2022/3/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
